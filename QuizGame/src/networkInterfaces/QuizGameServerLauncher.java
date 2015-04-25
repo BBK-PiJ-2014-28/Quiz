348e5aba66412e772295c30fd68e0c05fa45fcce
Annabel Jump <annabel.jump@gmail.com>
2015-04-25 17:07:18 +0100

    Moved Launcher and filled in code

---
+++ QuizGame/src/networkInterfaces/QuizGameServerLauncher.java
@@ -0,0 +1,33 @@
+package networkInterfaces;
+
+/**
+ * Created for PiJ Coursework 5
+ * @author Annabel Jump
+ * A Quiz Game
+ */
+
+import java.net.MalformedURLException;
+import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+
+
+public class QuizGameServerLauncher {
+
+	private void launch(){
+		if (System.getSecurityManager() == null){
+			System.setSecurityManager(new RMISecurityManager());
+		}
+		try {
+			LocateRegistry.createRegistry(1099);
+			QuizGameServer server = new QuizGameServer();
+			String registryHost = "//localhost/";
+			String serviceName = "quiz";
+			Naming.rebind(registryHost + serviceName, server);
+		} catch (MalformedURLException ex) {
+			ex.printStackTrace();
+		} catch (RemoteException ex){
+			ex.printStackTrace();
+		}
+	}
+	
+}
