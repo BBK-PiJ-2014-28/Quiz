a679fdc5b47867e6890cd217e61012e10e863842
Annabel Jump <annabel.jump@gmail.com>
2015-04-25 12:19:23 +0100

    Added javadoc and methods to Player Client interface

--- QuizGame/src/networkInterfaces/PlayerClient.java
+++ QuizGame/src/networkInterfaces/PlayerClient.java
@@ -6,6 +6,27 @@ package networkInterfaces;
  * A Quiz Game
  */
 
+import java.util.List;
+
 public interface PlayerClient {
 
+	/**
+	 * Gets a list of the available Quizzes for the player
+	 * @return List of quizzes
+	 */
+	List<?> getQuizList();
+	
+	/**
+	 * Shows the player a list of their scores
+	 * @return List of player's scores
+	 */
+	List<?> getPlayerScores();
+	
+	/**
+	 * Gets the Player's requested Quiz
+	 * Player can play quiz
+	 * Returns their score on the quiz
+	 * @return Player's score on chosen quiz
+	 */
+	int playQuiz();
 }
