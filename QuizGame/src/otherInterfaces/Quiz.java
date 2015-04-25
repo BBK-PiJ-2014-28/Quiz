edc2642e3fbde659143081ee5f2b3236ba91d345
Annabel Jump <annabel.jump@gmail.com>
2015-04-25 14:51:30 +0100

    Added some Quiz methods - when onto Questions and answers, have realised I need a Question class to implement the functionality I want

--- QuizGame/src/otherInterfaces/Quiz.java
+++ QuizGame/src/otherInterfaces/Quiz.java
@@ -7,5 +7,38 @@ package otherInterfaces;
  */
 
 public interface Quiz {
+	
+	/**
+	 * Get ID of quiz
+	 * @param name of quiz
+	 * @return the ID of the quiz
+	 */
+	int getQuizID(String name);
+	
+	/**
+	 * Get Name of quiz
+	 * @param ID of quiz
+	 * @return the name of the quiz
+	 */
+	String getQuizName(int ID);
+	
+	/**
+	 * Set a question in the quiz
+	 * @param the question, as a string
+	 */
+	void setQuestion(String question);
+	
+	/**
+	 * {Additional Content:
+	 * Change a question in the quiz
+	 * @param the question number that is to be changed
+	 * @param the new question as a string}
+	 */
+	void changeQuestion(int number, String question);
+	
+	/**
+	 * Set an answer to a question
+	 * @param 
+	 */
 
 }
