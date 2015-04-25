254a6296be239274442bfa83a76343079f762d95
Annabel Jump <annabel.jump@gmail.com>
2015-04-25 11:47:27 +0100

    Added blank interfaces and empty packages for organisation.

---
+++ QuizGame/src/networkInterfaces/PlayerClient.java
@@ -0,0 +1,11 @@
+package networkInterfaces;
+
+/**
+ * Created for PiJ Coursework 5
+ * @author Annabel Jump
+ * A Quiz Game
+ */
+
+public interface PlayerClient {
+
+}
---
+++ QuizGame/src/networkInterfaces/QuizGameServer.java
@@ -0,0 +1,14 @@
+package networkInterfaces;
+
+/**
+ * Created for PiJ Coursework 5
+ * @author Annabel Jump
+ * A Quiz Game
+ */
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface QuizGameServer extends Remote {
+
+}
