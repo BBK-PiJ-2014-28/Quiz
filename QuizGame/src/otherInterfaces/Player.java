3a03c14a9a62c20e7e3dd6f7a9d4e81b2de2a821
Annabel Jump <annabel.jump@gmail.com>
2015-04-25 14:37:08 +0100

    Added methods for implementation to Player interface

--- QuizGame/src/otherInterfaces/Player.java
+++ QuizGame/src/otherInterfaces/Player.java
@@ -6,7 +6,64 @@ package otherInterfaces;
  * A Quiz Game
  */
 
+import java.util.List;
 
 public interface Player {
+	
+	/**
+	 * Get Name of Player, given a player ID
+	 * @param player ID
+	 * @return the name of the player
+	 */
+	String getPlayerName(int iD);
+	
+	/**
+	 * Get iD of player, given their name
+	 * @param player name
+	 * @return the iD of the player
+	 */
+	int getPlayerID(String name);
+	
+	/**
+	 * Get score of player on quiz - two variants, with name, with iD
+	 * @param the ID of the quiz
+	 * @param the name of the player OR the ID of the player
+	 * @return the score of the player
+	 */
+	int getPlayerScore(int quizID, String playerName);
+	
+	int getPlayerScore(int quizID, int playerID);
+	
+	/**
+	 * {Additional Content:
+	 * Get all the scores of the player on all quizzes
+	 * @param the ID of the player OR the name of the player
+	 * @return a list of their scores}
+	 */
+	List<?> getAllScores(int playerID);
+	
+	List<?> getAllScores(String playerName);
+	
+	/**
+	 * {Additional Content:
+	 * Displays if high scorer on quiz
+	 * @param player name OR player ID
+	 * @param ID of quiz
+	 * @return Yes/No if player is high scorer}
+	 */
+	boolean isPlayerWinner(String playerName);
+	
+	boolean isPlayerWinner(int playerID);
+	
+	/**
+	 * {Additional Content:
+	 * Displays if player high scorer on ANY quiz
+	 * @param player name OR player ID
+	 * @return yes/no if player is a high scorer}
+	 */
+	boolean doesPlayerWinAtLife(String playerName);
+	
+	boolean doesPlayerWinAtLife(int playerID);
 
+	//TODO consider new class QuizMaster give Player a password to start creating quizzes
 }
