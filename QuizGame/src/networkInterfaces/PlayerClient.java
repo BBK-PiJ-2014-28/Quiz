77cae1e37fb5f420816bf16e98520224a009e4f3
Annabel Jump <annabel.jump@gmail.com>
2015-04-26 19:03:29 +0100

    Stuck on Playerclient. Will come back to it

--- QuizGame/src/networkInterfaces/PlayerClient.java
+++ QuizGame/src/networkInterfaces/PlayerClient.java
@@ -6,10 +6,36 @@ package networkInterfaces;
  * A Quiz Game
  */
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.util.List;
+
+import networkImpls.QuizGameServer;
+import networkImpls.QuizGameServerLauncher;
 
 public interface PlayerClient {
 
+	public static void main(String args[]){
+		PlayerClient launcher = new PlayerClient();
+		launcher.launch();
+	}
+	
+	private void launch(){
+		if (System.getSecurityManager() == null){
+			System.setSecurityManager(new SecurityManager());
+		}
+		try {
+			Remote service = Naming.lookup("//127.0.0.1:1099/quiz");
+			
+
+		} catch (MalformedURLException ex) {
+			ex.printStackTrace();
+		} catch (RemoteException ex){
+			ex.printStackTrace();
+		}
+	}
 	/**
 	 * Gets a list of the available Quizzes for the player
 	 * @return List of quizzes
